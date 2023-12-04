3f88eae235a82e0613f837b3394960a23fbf53c8
ZackthePuppy <daffodil_213123@yahoo.com>
2023-12-04 18:40:57 +0800

    initial version

---
+++ Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox3 = New TextBox()
+        one = New Button()
+        two = New Button()
+        three = New Button()
+        four = New Button()
+        five = New Button()
+        six = New Button()
+        seven = New Button()
+        eight = New Button()
+        nine = New Button()
+        zero = New Button()
+        divide = New Button()
+        clear = New Button()
+        minus = New Button()
+        multiply = New Button()
+        plus = New Button()
+        equals = New Button()
+        dot = New Button()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.BackColor = SystemColors.InfoText
+        TextBox3.Font = New Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox3.ForeColor = SystemColors.Window
+        TextBox3.Location = New Point(12, 41)
+        TextBox3.Name = "TextBox3"
+        TextBox3.ReadOnly = True
+        TextBox3.Size = New Size(226, 37)
+        TextBox3.TabIndex = 2
+        ' 
+        ' one
+        ' 
+        one.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        one.Location = New Point(12, 176)
+        one.Name = "one"
+        one.Size = New Size(40, 40)
+        one.TabIndex = 3
+        one.Text = "1"
+        one.UseVisualStyleBackColor = True
+        ' 
+        ' two
+        ' 
+        two.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        two.Location = New Point(58, 176)
+        two.Name = "two"
+        two.Size = New Size(40, 40)
+        two.TabIndex = 4
+        two.Text = "2"
+        two.UseVisualStyleBackColor = True
+        ' 
+        ' three
+        ' 
+        three.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        three.Location = New Point(104, 176)
+        three.Name = "three"
+        three.Size = New Size(40, 40)
+        three.TabIndex = 5
+        three.Text = "3"
+        three.UseVisualStyleBackColor = True
+        ' 
+        ' four
+        ' 
+        four.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        four.Location = New Point(12, 130)
+        four.Name = "four"
+        four.Size = New Size(40, 40)
+        four.TabIndex = 6
+        four.Text = "4"
+        four.UseVisualStyleBackColor = True
+        ' 
+        ' five
+        ' 
+        five.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        five.Location = New Point(58, 130)
+        five.Name = "five"
+        five.Size = New Size(40, 40)
+        five.TabIndex = 7
+        five.Text = "5"
+        five.UseVisualStyleBackColor = True
+        ' 
+        ' six
+        ' 
+        six.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        six.Location = New Point(104, 130)
+        six.Name = "six"
+        six.Size = New Size(40, 40)
+        six.TabIndex = 8
+        six.Text = "6"
+        six.UseVisualStyleBackColor = True
+        ' 
+        ' seven
+        ' 
+        seven.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        seven.Location = New Point(12, 84)
+        seven.Name = "seven"
+        seven.Size = New Size(40, 40)
+        seven.TabIndex = 9
+        seven.Text = "7"
+        seven.UseVisualStyleBackColor = True
+        ' 
+        ' eight
+        ' 
+        eight.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        eight.Location = New Point(58, 84)
+        eight.Name = "eight"
+        eight.Size = New Size(40, 40)
+        eight.TabIndex = 10
+        eight.Text = "8"
+        eight.UseVisualStyleBackColor = True
+        ' 
+        ' nine
+        ' 
+        nine.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        nine.Location = New Point(104, 84)
+        nine.Name = "nine"
+        nine.Size = New Size(40, 40)
+        nine.TabIndex = 11
+        nine.Text = "9"
+        nine.UseVisualStyleBackColor = True
+        ' 
+        ' zero
+        ' 
+        zero.Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        zero.Location = New Point(12, 222)
+        zero.Name = "zero"
+        zero.Size = New Size(86, 40)
+        zero.TabIndex = 12
+        zero.Text = "0"
+        zero.UseVisualStyleBackColor = True
+        ' 
+        ' divide
+        ' 
+        divide.Font = New Font("Showcard Gothic", 20F)
+        divide.Location = New Point(152, 84)
+        divide.Name = "divide"
+        divide.Size = New Size(40, 40)
+        divide.TabIndex = 13
+        divide.Text = "/"
+        divide.UseVisualStyleBackColor = True
+        ' 
+        ' clear
+        ' 
+        clear.Font = New Font("Showcard Gothic", 20F)
+        clear.Location = New Point(198, 84)
+        clear.Name = "clear"
+        clear.Size = New Size(40, 40)
+        clear.TabIndex = 14
+        clear.Text = "C"
+        clear.UseVisualStyleBackColor = True
+        ' 
+        ' minus
+        ' 
+        minus.Font = New Font("Showcard Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        minus.Location = New Point(152, 130)
+        minus.Name = "minus"
+        minus.Size = New Size(40, 40)
+        minus.TabIndex = 15
+        minus.Text = "-"
+        minus.UseVisualStyleBackColor = True
+        ' 
+        ' multiply
+        ' 
+        multiply.Font = New Font("Showcard Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        multiply.Location = New Point(198, 130)
+        multiply.Name = "multiply"
+        multiply.Size = New Size(40, 40)
+        multiply.TabIndex = 16
+        multiply.Text = "*"
+        multiply.UseVisualStyleBackColor = True
+        ' 
+        ' plus
+        ' 
+        plus.Font = New Font("Showcard Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        plus.Location = New Point(152, 176)
+        plus.Name = "plus"
+        plus.Size = New Size(40, 86)
+        plus.TabIndex = 17
+        plus.Text = "+"
+        plus.UseVisualStyleBackColor = True
+        ' 
+        ' equals
+        ' 
+        equals.Font = New Font("Showcard Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        equals.Location = New Point(198, 176)
+        equals.Name = "equals"
+        equals.Size = New Size(40, 86)
+        equals.TabIndex = 18
+        equals.Text = "="
+        equals.UseVisualStyleBackColor = True
+        ' 
+        ' dot
+        ' 
+        dot.Font = New Font("Showcard Gothic", 20F)
+        dot.Location = New Point(104, 222)
+        dot.Name = "dot"
+        dot.Size = New Size(40, 40)
+        dot.TabIndex = 19
+        dot.Text = "."
+        dot.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Showcard Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(219, 27)
+        Label1.TabIndex = 20
+        Label1.Text = "Simple Calculator"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(251, 271)
+        Controls.Add(Label1)
+        Controls.Add(dot)
+        Controls.Add(equals)
+        Controls.Add(plus)
+        Controls.Add(multiply)
+        Controls.Add(minus)
+        Controls.Add(clear)
+        Controls.Add(divide)
+        Controls.Add(zero)
+        Controls.Add(nine)
+        Controls.Add(eight)
+        Controls.Add(seven)
+        Controls.Add(six)
+        Controls.Add(five)
+        Controls.Add(four)
+        Controls.Add(three)
+        Controls.Add(two)
+        Controls.Add(one)
+        Controls.Add(TextBox3)
+        Name = "Form1"
+        Text = "Simple Calcu by Danette"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents one As Button
+    Friend WithEvents two As Button
+    Friend WithEvents three As Button
+    Friend WithEvents four As Button
+    Friend WithEvents five As Button
+    Friend WithEvents six As Button
+    Friend WithEvents seven As Button
+    Friend WithEvents eight As Button
+    Friend WithEvents nine As Button
+    Friend WithEvents zero As Button
+    Friend WithEvents divide As Button
+    Friend WithEvents clear As Button
+    Friend WithEvents minus As Button
+    Friend WithEvents multiply As Button
+    Friend WithEvents plus As Button
+    Friend WithEvents equals As Button
+    Friend WithEvents dot As Button
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+
+    Dim currentInput As String = ""
+    Dim lastButtonClicked As String = ""
+    Dim decimalAdded As Boolean = False
+
+    Private Sub NumberButtonClick(sender As Object, e As EventArgs) Handles zero.Click, one.Click, two.Click, three.Click, four.Click, five.Click, six.Click, seven.Click, eight.Click, nine.Click
+        'for clicking numbers
+        Dim numberButton As Button = CType(sender, Button)
+        currentInput += numberButton.Text
+        UpdateDisplay()
+        ' Reset the decimalAdded flag when a new digit is added
+        decimalAdded = False
+    End Sub
+
+    Private Sub DecimalButtonClick(sender As Object, e As EventArgs) Handles dot.Click
+        ' Check if the current number already contains a decimal point
+        If Not decimalAdded Then
+            ' If the last button clicked was an operator or none, allow decimal
+            If lastButtonClicked = "" OrElse lastButtonClicked = "+" OrElse lastButtonClicked = "-" OrElse lastButtonClicked = "*" OrElse lastButtonClicked = "/" Then
+                currentInput += "."
+                decimalAdded = True
+            Else
+                ' Check if the current number contains an operator
+                Dim parts() As String = currentInput.Split("+"c, "-"c, "*"c, "/"c)
+                Dim lastNumber As String = parts.Last()
+                If Not lastNumber.Contains(".") Then
+                    currentInput += "."
+                    decimalAdded = True
+                End If
+            End If
+            UpdateDisplay()
+        End If
+    End Sub
+
+
+    Private Sub UpdateDisplay()
+        ' Update the display with the current input
+        TextBox3.Text = currentInput
+    End Sub
+
+    Private Sub OperatorButtonClick(sender As Object, e As EventArgs) Handles plus.Click, minus.Click, divide.Click, multiply.Click
+        'for clicking operator
+        Dim operatorButton As Button = CType(sender, Button)
+        currentInput += operatorButton.Text
+        lastButtonClicked = operatorButton.Text
+        UpdateDisplay()
+    End Sub
+
+    Private Sub EqualsButtonClick(sender As Object, e As EventArgs) Handles equals.Click
+        ' Evaluate the expression and update the display
+        Try
+            Dim result = EvaluateExpression()
+            currentInput = result.ToString()
+            UpdateDisplay()
+        Catch ex As Exception
+            currentInput = "ERROR"
+            UpdateDisplay()
+        End Try
+    End Sub
+
+    Private Function EvaluateExpression() As Double
+        ' Use DataTable.Compute to evaluate the expression
+        Dim table As New DataTable()
+        Dim result As Object = table.Compute(currentInput, "")
+        Return Convert.ToDouble(result)
+    End Function
+
+    Private Sub ClearButtonClick(sender As Object, e As EventArgs) Handles clear.Click
+        ' Clear the current input
+        currentInput = ""
+        lastButtonClicked = ""
+        UpdateDisplay()
+    End Sub
+
+End Class
